a371ab268297aa79956fa85dbab96bcfd0c4503d
Rango Mango <mangorango144@gmail.com>
2025-01-13 21:08:42 +0200

    feat: add ErrorPage

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,11 +2,13 @@ import { Products } from "../routes";
 import { ShoppingCart } from "../routes";
 import { Shop } from "../routes";
 import { Home } from "../routes";
+import { ErrorPage } from "../routes";
 
 const routes = [
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "shop",
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from "./Home";
+export { default as ErrorPage } from "./ErrorPage";
 export * from "./Shop";
