34565fb509da34718330a84865305720fa741f34
Rango Mango <mangorango144@gmail.com>
2025-01-23 21:07:14 +0200

    feat: add My Cart link static structure to Navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center bg-white shadow-md mx-auto mt-20 mb-4 py-4 rounded-full w-[95%] xl:w-8/12 text-black">
+    <footer className="flex justify-center bg-white shadow-md mx-auto mt-32 mb-4 py-4 rounded-full w-[95%] xl:w-8/12 text-black">
       <p>
         &copy; {new Date().getFullYear()} Shopping Cart. All rights reserved.
       </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
+import { BsCart4 } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-start space-x-5 bg-white shadow-md mx-auto my-4 px-8 py-4 rounded-full w-[95%] xl:w-8/12 font-medium text-black">
+    <nav className="flex justify-start items-center bg-white shadow-md mx-auto mt-4 px-14 rounded-full w-[95%] xl:w-8/12 h-[56px] font-medium text-black">
       <Link to="/">Home</Link>
-      <Link to="shop/products">Products</Link>
-      <Link to="shop/shopping-cart">Shopping Cart</Link>
+      <Link to="shop/products" className="ml-5">
+        Products
+      </Link>
+      <Link
+        to="shop/shopping-cart"
+        className="flex items-center hover:bg-lime-200 ml-auto px-4 py-1 rounded-2xl transition duration-500"
+      >
+        <BsCart4 className="mr-2 w-auto h-6" />
+        <span className="flex flex-col text-xs">
+          My Cart
+          <span className="text-slate-400">3 Items</span>
+        </span>
+      </Link>
     </nav>
   );
 }
