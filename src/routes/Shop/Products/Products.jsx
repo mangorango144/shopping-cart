ce8267b261f100367e4afcc8cc756874b1ef67c6
Rango Mango <mangorango144@gmail.com>
2025-01-25 19:38:16 +0200

    feat: extract loader into ProductsLoader

--- src/routes/Shop/Products/Products.jsx
+++ src/routes/Shop/Products/Products.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import { ProductsLoader } from "../../../components";
 
 export default function Products() {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,14 +14,7 @@ export default function Products() {
   return (
     <>
       {isLoading ? (
-        <>
-          <div className="flex justify-center items-center h-96">
-            <div className="border-4 mr-4 border-t-transparent border-teal-500 rounded-full w-12 h-12 animate-spin"></div>
-            <p className="bg-clip-text bg-gradient-to-b from-teal-300 to-teal-600 font-bold text-2xl text-center text-transparent">
-              Loading...
-            </p>
-          </div>
-        </>
+        <ProductsLoader />
       ) : (
         <section className="gap-x-12 gap-y-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 m-auto w-[95%] xl:w-8/12">
           <ProductCard />
