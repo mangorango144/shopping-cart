b665ef764ea2435c06684ab22066e12d4dd9e833
Rango Mango <mangorango144@gmail.com>
2025-01-27 17:03:49 +0200

    feat: add error handling in Products component
    Also some restructuring of the return statement.

--- src/routes/Shop/Products/Products.jsx
+++ src/routes/Shop/Products/Products.jsx
@@ -3,16 +3,27 @@ import ProductCard from "./ProductCard";
 import { ProductsLoader } from "../../../components";
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getProducts = () => {
       setTimeout(async () => {
-        const res = await fetch("https://fakestoreapi.com/products");
-        const json = await res.json();
-        setProducts(json);
-        setIsLoading(false);
+        try {
+          const res = await fetch("https://fakestoreapi.com/products");
+          if (!res.ok) {
+            throw new Error(`HTTP error: Status ${res.status}`);
+          }
+          const json = await res.json();
+          setProducts(json);
+          setError(null);
+        } catch (err) {
+          setError(err.message);
+          setProducts(null);
+        } finally {
+          setLoading(false);
+        }
       }, 500);
     };
 
@@ -21,9 +32,13 @@ export default function Products() {
 
   return (
     <>
-      {isLoading ? (
-        <ProductsLoader />
-      ) : (
+      {loading && <ProductsLoader />}
+      {error && (
+        <div className="flex justify-center my-auto font-semibold text-red-500 text-xl">
+          {error}
+        </div>
+      )}
+      {products && (
         <section className="gap-x-12 gap-y-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 m-auto w-[95%] xl:w-8/12">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
