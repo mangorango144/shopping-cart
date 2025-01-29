3b8a20c466efa57f1e18d9450317ddc889d7c9bd
Rango Mango <mangorango144@gmail.com>
2025-01-30 01:04:17 +0200

    feat: add remove for OrderCard
    Also add logic for <hr> placement.

--- src/routes/Shop/ShoppingCart/Order.jsx
+++ src/routes/Shop/ShoppingCart/Order.jsx
@@ -9,10 +9,10 @@ export default function Order() {
     <div>
       <span className="block mb-5 font-medium text-2xl">Order</span>
       <div className="items-center gap-5 grid grid-cols-[1fr_3fr_0.5fr_2fr] bg-white shadow-md p-9 rounded-xl">
-        {cart.map((product) => (
+        {cart.map((product, index) => (
           <Fragment key={product.id}>
             <OrderCard product={product} />
-            <hr className="col-span-4" />
+            {index < cart.length - 1 && <hr className="col-span-4" />}
           </Fragment>
         ))}
       </div>
--- src/routes/Shop/ShoppingCart/OrderCard.jsx
+++ src/routes/Shop/ShoppingCart/OrderCard.jsx
@@ -1,7 +1,13 @@
 import { useShop } from "../../../hooks";
+import { TbTrashOff } from "react-icons/tb";
 
 export default function OrderCard({ product }) {
-  const { cart } = useShop();
+  const { cart, setCart } = useShop();
+
+  const handleRemove = (productToRemove) => () => {
+    setCart(cart.filter((curr) => curr.id != productToRemove.id));
+  };
+
   return (
     <>
       <img src={product.image} alt={product.title} className="" />
@@ -9,14 +15,20 @@ export default function OrderCard({ product }) {
         {product.title}
       </p>
       <p className="font-bold text-[8px] sm:text-base">${product.price}</p>
-      <div className="flex justify-center items-center space-x-2">
+      <div className="flex justify-center items-center">
         <p className="text-[8px] text-slate-400 sm:text-sm">Quantity:</p>
         <input
           type="number"
           name="quantity"
           defaultValue="1"
-          className="border rounded-lg w-10 text-center"
+          className="ml-1 border rounded-lg w-10 text-center"
         />
+        <div
+          onClick={handleRemove(product)}
+          className="flex justify-center hover:border-2 hover:bg-red-100 ml-6 border-red-200 rounded-lg w-8 cursor-pointer"
+        >
+          <TbTrashOff className="text-red-600 text-sm md:text-xl" />
+        </div>
       </div>
     </>
   );
