0bddd3f8bb30a39abe3f4fcf6845d094f08a2f34
Rango Mango <mangorango144@gmail.com>
2025-02-28 10:53:01 +0200

    refactor: add safeguard for useShop hook usage

--- src/hooks/useShop.js
+++ src/hooks/useShop.js
@@ -1,4 +1,10 @@
 import { useContext } from "react";
 import { ShopContext } from "../context";
 
-export const useShop = () => useContext(ShopContext);
+export const useShop = () => {
+  const context = useContext(ShopContext);
+  if (!context) {
+    throw new Error("useShop must be used within a ShopProvider");
+  }
+  return context;
+};
